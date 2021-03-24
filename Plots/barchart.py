cbc58b8b58dd2708b89b27de21a5bb882d9c032d
MarsProgrammingLab <martavias.cary@outlook.com>
2021-03-24 02:05:45 -0400

    visualization lab part two

--- Plots/barchart.py
+++ Plots/barchart.py
@@ -3,27 +3,38 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 
 # Load CSV file from Datasets folder
-df = pd.read_csv('../Datasets/CoronavirusTotal.csv')
-
-# Filtering US Cases
-filtered_df = df[df['Country'] == 'US']
+df = pd.read_csv('C:/Users/marta/OneDrive/Desktop/Coursework/SoftwareEngineeringITCS-3315/datasets/CoronavirusTotal.csv')
 
 # Removing empty spaces from State column to avoid errors
-filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-# Creating sum of number of cases group by State Column
-new_df = filtered_df.groupby(['State'])['Confirmed'].sum().reset_index()
+# Creating unrecovered column
+df['Unrecovered'] = df['Confirmed'] - df['Deaths'] - df['Recovered']
 
-# Sorting values and select first 20 states
-new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
+# Removing China and Others from data frame
+df = df[(df['Country'] != 'China')]
+
+# Creating sum of number of cases group by Country Column
+new_df = df.groupby(['Country']).agg(
+    {'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
+
+# Sorting values and select 20 first value
+new_df = new_df.sort_values(by=['Confirmed'],
+                            ascending=[False]).head(20).reset_index()
 
 # Preparing data
-data = [go.Bar(x=new_df['State'], y=new_df['Confirmed'])]
+trace1 = go.Bar(x=new_df['Country'], y=new_df['Unrecovered'], name='Unrecovered',
+                marker={'color': '#CD7F32'})
+trace2 = go.Bar(x=new_df['Country'], y=new_df['Recovered'], name='Recovered',
+                marker={'color': '#9EA0A1'})
+trace3 = go.Bar(x=new_df['Country'], y=new_df['Deaths'], name='Deaths',
+                marker={'color': '#FFD700'})
+data = [trace1, trace2, trace3]
 
 # Preparing layout
-layout = go.Layout(title='Corona Virus Confirmed Cases in The US', xaxis_title="States",
-                   yaxis_title="Number of confirmed cases")
+layout = go.Layout(title='Corona Virus Cases in the first 20 country expect China', xaxis_title="Country",
+                   yaxis_title="Number of cases", barmode='stack')
 
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename='barchart.html')
+pyo.plot(fig, filename='stackbarchart.html')
